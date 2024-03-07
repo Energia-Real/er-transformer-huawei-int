d75f2a94fadd88c51df98ff9dbf271e7b8ccefc3
Antonio Contreras <antonio@energiareal.mx>
2024-03-07 14:23:18 -0600

    se agrega endpoint para obtener solo el resumen de una planta

--- er-transformer-huawei-int/EndPoints/HuaweiEndpoints.cs
+++ er-transformer-huawei-int/EndPoints/HuaweiEndpoints.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using er_transformer_huawei_int.Models;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace er_transformer_huawei_int.EndPoints
 {
@@ -18,50 +20,32 @@ namespace er_transformer_huawei_int.EndPoints
 
             GetPlantListMethod(routeBuilder);
             GetDevListMethod(routeBuilder);
+            GetPlantMethod(routeBuilder);
         }
 
         private static void GetPlantListMethod(RouteGroupBuilder rgb)
         {
             rgb.MapPost("/GetPlantList", async (HttpContext context, [FromBody] int PageNo) =>
             {
-                var xsrfToken = await GetXsrfTokenAsync();
+                var plantList = await GetPlantList();
 
-                if (string.IsNullOrEmpty(xsrfToken) || xsrfToken.Contains("Error"))
+                if (plantList is null)
                 {
-                    return Results.Unauthorized();
-                }
-
-                var url = "https://la5.fusionsolar.huawei.com/thirdData/stations";
-                var jsonRequest = new { pageNo = PageNo };
-
-                using var client = new HttpClient();
-                client.DefaultRequestHeaders.Add("XSRF-TOKEN", xsrfToken);
-                var content = new StringContent(JsonSerializer.Serialize(jsonRequest), Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(url, content);
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var deserializeData = Newtonsoft.Json.JsonConvert.DeserializeObject<PlantList>(jsonResponse);
-                    return Results.Ok(deserializeData);
-                }
-                else
-                {
-                    await context.Response.WriteAsync($"Error: {response.StatusCode}");
                     return Results.NoContent();
                 }
+
+                return Results.Ok(plantList);
             })
             .Produces(200, typeof(PlantList))
             .Produces(204)
             .WithTags("huawei")
             .WithName("GetPlantList")
-            .RequireAuthorization()
             .WithOpenApi();
         }
 
         private static void GetDevListMethod(RouteGroupBuilder rgb)
         {
-            rgb.MapPost("/getDevList", async (HttpContext context, [FromBody] string StationCodes) =>
+            rgb.MapPost("/getDevList", async (HttpContext context, [FromBody] string StationCode) =>
             {
                 var xsrfToken = await GetXsrfTokenAsync();
 
@@ -71,7 +55,7 @@ namespace er_transformer_huawei_int.EndPoints
                 }
 
                 var url = "https://la5.fusionsolar.huawei.com/thirdData/getDevList";
-                var jsonRequest = new { stationCodes = StationCodes };
+                var jsonRequest = new { stationCodes = StationCode };
 
                 using var client = new HttpClient();
                 client.DefaultRequestHeaders.Add("XSRF-TOKEN", xsrfToken);
@@ -94,10 +78,65 @@ namespace er_transformer_huawei_int.EndPoints
             .Produces(204)
             .WithTags("huawei")
             .WithName("getDevList")
-            .RequireAuthorization()
             .WithOpenApi();
         }
 
+
+        private static void GetPlantMethod(RouteGroupBuilder rgb)
+        {
+            rgb.MapGet("/GetPlant", async (HttpContext context, [FromHeader] string StationCode) =>
+            {
+                var plantList = await GetPlantList();
+
+                if (plantList is null)
+                {
+                   return Results.NoContent();
+                }
+
+                var filteredPlant = plantList.Data.List.FirstOrDefault(a=>a.plantCode == StationCode);
+
+                if (filteredPlant is null)
+                {
+                    return Results.NoContent();
+                }
+
+               return Results.Ok(filteredPlant);
+            })
+            .Produces(200, typeof(PlantList))
+            .Produces(204)
+            .WithTags("huawei")
+            .WithName("GetPlant")
+            .WithOpenApi();
+        }
+
+
+        private static async Task<PlantList> GetPlantList()
+        {
+            var xsrfToken = await GetXsrfTokenAsync();
+
+            if (string.IsNullOrEmpty(xsrfToken) || xsrfToken.Contains("Error"))
+            {
+                return null;
+            }
+
+            var url = "https://la5.fusionsolar.huawei.com/thirdData/stations";
+            var jsonRequest = new { pageNo = 1 };
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("XSRF-TOKEN", xsrfToken);
+            var content = new StringContent(JsonSerializer.Serialize(jsonRequest), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<PlantList>(jsonResponse);
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         private static async Task<string> GetXsrfTokenAsync()
         {
